e3259bf0df57a4e58e5f76071ef35a4e7b6d9d5a
Lucas Baggio <lucas.baggio.silva@gmail.com>
2026-02-11 17:52:28 -0300

    feat: add SchoolController with CRUD endpoints

---
+++ src/modules/academic/presentation/school.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { CreateSchoolUseCase } from '../application/use-cases/create-school.use-case';
+import { GetSchoolByIdUseCase } from '../application/use-cases/get-school-by-id.use-case';
+import { ListActiveSchoolsUseCase } from '../application/use-cases/list-active-schools.use-case';
+import { GetSchoolsByAdminUseCase } from '../application/use-cases/get-school-by-admin-id.use-case';
+import { CreateSchoolDto } from '../application/dtos/create-school.dto';
+
+@Controller('schools')
+export class SchoolController {
+  constructor(
+    private readonly createSchoolUseCase: CreateSchoolUseCase,
+    private readonly getSchoolByIdUseCase: GetSchoolByIdUseCase,
+    private readonly listAllActiveSchoolsUseCase: ListActiveSchoolsUseCase,
+    private readonly getSchoolsByAdminUseCase: GetSchoolsByAdminUseCase,
+  ) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() dto: CreateSchoolDto) {
+    const school = await this.createSchoolUseCase.execute(dto);
+    return {
+      id: school.id,
+      name: school.name,
+      adminId: school.adminId,
+      isActive: school.isActive,
+    };
+  }
+
+  @Get()
+  async listAll() {
+    const schools = await this.listAllActiveSchoolsUseCase.execute();
+    return schools.map((school) => ({
+      id: school.id,
+      name: school.name,
+      adminId: school.adminId,
+      isActive: school.isActive,
+    }));
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string) {
+    return await this.getSchoolByIdUseCase.execute(id);
+  }
+
+  @Get('admin/:adminId')
+  async getByAdmin(@Param('adminId') adminId: string) {
+    return await this.getSchoolsByAdminUseCase.execute(adminId);
+  }
+}
