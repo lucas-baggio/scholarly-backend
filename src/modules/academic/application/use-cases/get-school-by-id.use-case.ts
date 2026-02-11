c9217df59b77d95dbfad98de32bb416b3691317d
Lucas Baggio <lucas.baggio.silva@gmail.com>
2026-02-11 17:52:12 -0300

    feat: add GetSchoolByIdUseCase

---
+++ src/modules/academic/application/use-cases/get-school-by-id.use-case.ts
@@ -0,0 +1,16 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { SchoolRepository } from '../../domain/school.repository';
+import { School } from '../../domain/school.entity';
+
+@Injectable()
+export class GetSchoolByIdUseCase {
+  constructor(private readonly schoolRepository: SchoolRepository) {}
+
+  async execute(id: string): Promise<School | null> {
+    const school = await this.schoolRepository.findById(id);
+    if (!school) {
+      throw new NotFoundException(`School with ID ${id} not found`);
+    }
+    return school;
+  }
+}
