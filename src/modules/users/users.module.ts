8ea0831ff44ac1c4f0482f33c2493bb80067b446
Lucas Baggio <lucas.baggio.silva@gmail.com>
2026-02-11 01:18:34 -0300

    feat: add use cases providers to UserModules

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,12 +5,18 @@ import { HashService } from 'src/@shared/application/crypto/hash.service';
 import { BcryptHashService } from 'src/@shared/infrastructure/crypto/bcrypt-hash.service';
 import { InMemoryUserRepository } from './infrastructure/persistence/in-memory-user.repository';
 import { UserController } from './presentation/user.controller';
+import { ListActiveUsersUseCase } from './application/use-cases/list-active-users.use-case';
+import { GetUserByIdUseCase } from './application/use-cases/get-user-by-id.use-case';
+import { DeactivateUserUseCase } from './application/use-cases/deactivate-user.use-case';
 
 @Module({
   imports: [],
   controllers: [UserController],
   providers: [
     CreateUserUseCase,
+    ListActiveUsersUseCase,
+    GetUserByIdUseCase,
+    DeactivateUserUseCase,
     {
       provide: UserRepository,
       useClass: InMemoryUserRepository,
